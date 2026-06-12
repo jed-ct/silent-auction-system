9208a3be6da31ddadfc4ad23a40cc046cd8a9c0d
jed-ct <tuquerojedric@gmail.com>
2026-06-12 21:29:13 +0800

    feat: added base item class

---
+++ src/com/auction/models/Item.java
@@ -0,0 +1,11 @@
+package com.auction.models;
+
+public class Item {
+    private String itemName;
+    private int quantity;
+
+    public Item(String itemName, int quantity) {
+        this.itemName = itemName;
+        this.quantity = quantity;
+    }
+}
