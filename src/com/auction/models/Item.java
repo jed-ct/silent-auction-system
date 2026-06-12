67e037fbd4c60805bf2a1a0592bbd6ed728b884b
jed-ct <tuquerojedric@gmail.com>
2026-06-12 22:25:37 +0800

    feat: implement lot id in item class

--- src/com/auction/models/Item.java
+++ src/com/auction/models/Item.java
@@ -1,12 +1,21 @@
 package com.auction.models;
 
 public class Item {
+    private final int lotId;
     private final String itemName;
     private final int quantity;
+
+    private static int lotIdCounter = 0;
 
     public Item(String itemName, int quantity) {
         this.itemName = itemName;
         this.quantity = quantity;
+        lotId = lotIdCounter;
+        lotIdCounter++;
+    }
+
+    public int getLotId() {
+        return lotId;
     }
 
     public String getItemName() {
